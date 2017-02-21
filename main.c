41357dc1de37d9fdea43c15b5429690d9bc92ceb
VagueFuture <igrokart97@mail.ru>
2017-02-21 20:08:13 +0700

    Proverka Vvodimih dannih

--- main.c
+++ main.c
@@ -2,5 +2,15 @@
 #include <stdlib.h>
  int main()
  {
- 	
+ 	float srok=-1,summ=-1,itog,p=0;
+////////////////////////////////////////
+ 	printf("Vvegite srok vklada \n");
+ while(srok<0 || srok>365) scanf("%f",&srok);
+ ///////////////////////////////////////
+ 		printf("Vvegite summu vklada MIN 10000p\n");
+ while(summ<10000) scanf("%f",&summ);	
+ ////////////////////////////////////////
+
+
+ return(0);	
  }
