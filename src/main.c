8e5dfdac101bfb0728c9d55cd8c576e6d700fd5c
VagueFuture <igrokart97@mail.ru>
2017-04-19 14:30:12 +0700

    Chasti_Programmi

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "deposit.h" 
+//#include "deposit.c"
+
+int main()
+ {
+ 	double srok=-1,summ=-1;
+////////////////////////////////////////
+    srok=provsrok(srok);
+////////////////////////////////////////
+ 	summ=provsumm(summ);
+/////////////////////////////////////////
+ sravnenie(srok,summ);
+ 
+ 	
+ return(0);	
+ }
